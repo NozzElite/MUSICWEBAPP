597cde6b7239236b5693f916d1521c3c5e9d4184
Shorya Rajput <88054009+NozzElite@users.noreply.github.com>
2022-05-11 16:46:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+song1="";
+song2="";
 
 function setup()
 {
     canvas=createCanvas(600,500);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function preload()
+{
+    song1=loadSound("Perfect.mp3");
+    song2=loadSound("harrypoter.mp3");
+}
+function draw()
+{
+     image(video, 0, 0, 600, 500);
+}
+function play()
+{
+    song1.play();
 }
